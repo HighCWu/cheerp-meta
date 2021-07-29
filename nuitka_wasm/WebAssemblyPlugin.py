33305bf3ab3dfc7250dd3420836810931c00c3e2
吴何聪 <HighCWu@163.com>
2021-07-29 13:50:59 +0800

    Update WebAssemblyPlugin.py

--- nuitka_wasm/WebAssemblyPlugin.py
+++ nuitka_wasm/WebAssemblyPlugin.py
@@ -43,7 +43,7 @@ class NuitkaPluginWebAssemblyWorkarounds(NuitkaPluginBase):
         cc_flags += " -I/usr/include/"
         cc_flags += " -I/usr/include/x86_64-linux-gnu/"
         # cc_flags += " --target=wasm64"
-        cc_flags += "-target cheerp"
+        cc_flags += " -target cheerp"
         os.environ["CCFLAGS"] = cc_flags
 
     @staticmethod
